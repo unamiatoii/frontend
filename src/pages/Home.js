336725044aa39b12446cc9d3638caedf23c18461
unamiatoii <pierrejaphetkonan@gmail.com>
2024-06-05 10:41:59 +0000

    FIn des modifs de la V1.0.0 , Tout foctionnel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,40 +1,48 @@
 // src/Home.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { ref, onValue, off } from 'firebase/database';
+import { database } from './../database/firebase';
 import './Home.css';
+import logo from '../assets/Logo_wtbg.png';
 
 const Home = () => {
   const navigate = useNavigate();
+  const [chattingCount, setChattingCount] = useState(0);
+
+  useEffect(() => {
+    const usersRef = ref(database, 'users');
+    const unsubscribe = onValue(usersRef, (snapshot) => {
+      const users = snapshot.val();
+      const activeUsers = Object.keys(users).filter(id => users[id].chatting);
+      setChattingCount(activeUsers.length);
+    });
+
+    // Clean up the subscription
+    return () => off(usersRef, unsubscribe);
+  }, []);
 
   const startChat = () => {
     navigate('/chat');
   };
 
   return (
-    <div className="container p-4 d-flex flex-column justify-content-center align-items-center text-center" style={styles.container}>
-      <h3 className="mb-4" style={styles.title}>
+    <div className="home-container">
+      <div className="logo-container">
+        <img src={logo} alt="Logo" className="logo" />
+      </div>
+      <h3 className="title">
         Faites-vous des ami(e)s, et bien plus encore, partout et à tout moment
       </h3>
-      <button className="btn btn-dark btn-lg" onClick={startChat} style={styles.button}>
+      <button className="btn btn-dark btn-lg start-chat-button" onClick={startChat}>
         Commencer à chatter
       </button>
+      <div className="chatting-count mt-4">
+        <h1>{chattingCount}</h1>
+        <h2>Personnes connectées</h2>
+      </div>
     </div>
   );
-};
-
-const styles = {
-  container: {
-    height: '100vh',
-  },
-  title: {
-    color: 'var(--color-yellow)',
-    fontSize: '2rem',
-  },
-  button: {
-    backgroundColor: 'var(--color-yellow)',
-    borderColor: 'var(--color-yellow)',
-    color: 'var(--color-black)',
-  },
 };
 
 export default Home;
